e3e2bdaec241dcd1215d650d315a9151c1d03b11
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-09-10 09:22:22 -0300

    nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Layout from './components/Layout';
+import Navbar from './components/Navbar';
 import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
+      <Layout>
+        <Navbar />
+      </Layout>
+
       <GlobalStyles />
     </>
-  );
+  ); 
 }
 
 export default App;
